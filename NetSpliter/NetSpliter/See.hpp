18af790e8a57185c7469df8144093bda092d8f11
Vadimador <vadgolda@gmail.com>
2020-04-15 01:18:46 +0200

    Mise à jour des tests de  la commande "see"
    D'après moi (fatigué donc je me trompe surement) tout les tests pour la commande en question on été fait, ptet pas de la manière la plus optimisé ,mais au moins on peut pas écrire n'importe quoi

--- NetSpliter/NetSpliter/See.hpp
+++ NetSpliter/NetSpliter/See.hpp
@@ -1,9 +1,15 @@
 #pragma once
 #include "Commande.hpp"
 
+/// Cette commande sert à inspecter un sous-réseau ou une machine
+///////////////////////////
 class See : public Commande
 {
 private :
+
+	int nbBitMasqueSousRéseau = 8; // représente le nombre de bit de la parti réseau de l'adresse IP si elle est valide 
+	int nbBitsReseau = 8; // représente le nombre de bit du masque réseau (et pas sous réseau !)
+
 	bool isMasqueSousRéseau(IP& i);
 public :
 	/// ip : l'adresse du réseau
